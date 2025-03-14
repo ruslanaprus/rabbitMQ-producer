4c0b9fcb21177bc7352ec4e92a27851a5b5459e7
ruslana <ruslana.prus@ukr.net>
2025-03-14 18:48:48 +0100

    schedule messages

--- src/main/java/com/peach/rabbitmq/producer/Application.java
+++ src/main/java/com/peach/rabbitmq/producer/Application.java
@@ -3,9 +3,11 @@ package com.peach.rabbitmq.producer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+@EnableScheduling
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
---
+++ src/main/java/com/peach/rabbitmq/producer/FixedRateProducer.java
@@ -0,0 +1,26 @@
+package com.peach.rabbitmq.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FixedRateProducer {
+    private static final Logger log = LoggerFactory.getLogger(FixedRateProducer.class);
+    private final RabbitTemplate rabbitTemplate;
+
+    public FixedRateProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    private int i = 0;
+
+    @Scheduled(fixedRate = 1000)
+    public void sendMessage(){
+        i++;
+        log.info("i is: {}", i);
+        rabbitTemplate.convertAndSend("peach.fixrate", "fixed rate: " + i);
+    }
+}
